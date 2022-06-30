eeea11292d0a9d6182a958067dee3d1028c9d52b
Jasurbek <jasurisoqov685@gmail.com>
2022-06-30 20:10:11 +0500

    UseRefExample1

--- src/Components/UseRefExample1.jsx
+++ src/Components/UseRefExample1.jsx
@@ -1,13 +1,44 @@
 import { useRef } from "react";
 
 const UseRefExample1 = () => {
+  const inputRef = useRef();
+  const paraRef = useRef();
+  const paraRef2 = useRef();
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    console.log(inputRef.current.value);
+
+    console.log(paraRef.current.innerText);
+
+    console.log(paraRef2.current.innerText);
+
+    // inputRef.current.style.backgroundColor = `${inputRef.current.value}`;
+
+    // paraRef.current.innerText = inputRef.current.value;
+
+    // inputRef.current.value = "";
+  };
+
   return (
-    <form className="form1">
-      <input type="text" name="name" id="name" className="input1" />
-      <button type="submit" className="btn1">
-        Click
-      </button>
-    </form>
+    <>
+      <form className="form1" onSubmit={onSubmit}>
+        <input
+          type="text"
+          name="name"
+          id="name"
+          className="input1"
+          ref={inputRef}
+        />
+        <button type="submit" className="btn1">
+          Click
+        </button>
+        <h2 ref={paraRef}>Assalomu alaykum</h2>
+      </form>
+
+      <h2 ref={paraRef2}>Not form</h2>
+    </>
   );
 };
 
