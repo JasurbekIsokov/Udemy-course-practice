548ddcc1fe3c194fedbced81630a7af490b7656d
Jasurbek <jasurisoqov685@gmail.com>
2022-06-30 21:43:48 +0500

    UseMemo

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import UseRefExample1 from "./Components/UseRefExample1";
 import UseRefExample2 from "./Components/UseRefExample2";
 import UseRefExample3 from "./Components/UseRefExample3";
+import UseMemo from "./Components/UseMemo";
 
 function App() {
   return (
     <>
       {/* <UseRefExample1 /> */}
       {/* <UseRefExample2 /> */}
-      <UseRefExample3 />
+      {/* <UseRefExample3 /> */}
+      <UseMemo />
     </>
   );
 }
