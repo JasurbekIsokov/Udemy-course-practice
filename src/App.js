92a4aa800d39e25ed3ec5863f13cec47f7b2b828
Jasurbek <jasurisoqov685@gmail.com>
2022-07-03 05:19:05 +0500

    create useFetch file jsx and import in App js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import UseRefExample2 from "./Components/UseRefExample2";
 import UseRefExample3 from "./Components/UseRefExample3";
 import UseMemo from "./Components/UseMemo";
 
+import CostumeHooksExample1 from "./Components/CostumeHooksExample1";
+
 function App() {
   return (
     <>
@@ -10,6 +12,7 @@ function App() {
       {/* <UseRefExample2 /> */}
       {/* <UseRefExample3 /> */}
       {/* <UseMemo /> */}
+      <CostumeHooksExample1 />
     </>
   );
 }
---
+++ src/Components/CostumeHooksExample1.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CostumeHooksExample1 = () => {
+  return <div>useFetch</div>;
+};
+
+export default CostumeHooksExample1;
