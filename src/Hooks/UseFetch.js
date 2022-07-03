45daec7ca4c5b33a2a9467b28687fe2ede2b4fac
Jasurbek <jasurisoqov685@gmail.com>
2022-07-03 05:10:33 +0500

    Create new folder Hooks and new file UseFetch and create variable

---
+++ src/Hooks/UseFetch.js
@@ -0,0 +1,9 @@
+import { useEffect, useState } from "react";
+
+const UseFetch = () => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+};
+
+export default UseFetch;
