2bb4364efaa249083613f1811284b001612471e4
Jasurbek <jasurisoqov685@gmail.com>
2022-07-07 07:04:02 +0500

    localStorega obekt qilib malumotlarni saqlash

--- src/Components/CustomHookExample2.jsx
+++ src/Components/CustomHookExample2.jsx
@@ -1,16 +1,30 @@
-import { useState } from "react";
+import useLocalStorage from "../Hooks/useLocalStorage";
 
 const CustomHookExample2 = () => {
-  const [task, setTask] = useState("");
+  const [task, setTask] = useLocalStorage("task", "");
+  const [tasks, setTasks] = useLocalStorage("tasks", []);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    const taskObj = {
+      task,
+      completed: false,
+      date: new Date().toLocaleDateString(),
+    };
+
+    setTasks([...task, taskObj]);
+  };
 
   return (
-    <form>
+    <form onSubmit={onSubmit}>
       <label htmlFor="text">Task</label>
       <input
         type="text"
         value={task}
         onChange={(e) => setTask(e.target.value)}
       />
+      <button>Submit</button>
     </form>
   );
 };
--- src/Hooks/useLocalStorage.js
+++ src/Hooks/useLocalStorage.js
@@ -1,9 +1,22 @@
 import { useState } from "react";
 
 const useLocalStorage = (key, initialValue) => {
-  const [localStorageValue, setLocalStorageValue] = useState(() => {
-    getLocalStorageValue(key, initialValue);
-  });
+  const [localStorageValue, setLocalStorageValue] = useState(() =>
+    getLocalStorageValue(key, initialValue)
+  );
+
+  const setValue = (value) => {
+    console.log(value);
+    // CHek if function
+    const valueToStore =
+      value instanceof Function ? value(localStorageValue) : value;
+
+    // Set to state
+    setLocalStorageValue(value);
+
+    // Set to local storage
+    localStorage.setItem(key, JSON.stringify(valueToStore));
+  };
 
   return [localStorageValue, setValue];
 };
